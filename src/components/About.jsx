8f0e7f3f77413fdb1e9ac4db1f1879394cc6eb3a
haroldCoder <haroldc2005@gmail.com>
2023-04-18 12:41:34 -0500

    about changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,15 @@ class About extends react.Component {
             <div className="about-section">
                 <div className="inner-container">
                     <h1 className='text-blue-500'>About Us</h1>
-                    <p className='text-white'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis,
-                    veritatis ut! Dolor dolorem quasi temporibus sequi, minus dolores similique
-                    dolore. Laboriosam provident, exercitationem quae labore aliquam odit nisi
-                    quas nulla.</p>
+                    <p className='text-white'>In this web application, the content used is merely a copy of the company Airbnb, with the aim of personal development in software development. Therefore, this project does not aim to compete with the company Airbnb. It should be noted that the use of Airbnb's content is for educational and personal development purposes only and is not intended to be used illegally or without permission.
+
+It is important to highlight that the use of content for educational and personal development purposes is a common practice in the software and technology industry and can be beneficial for individual learning and industry innovation.</p>
                     <div className="skills">
                         <span className='text-blue-400'>
                             Web design
                         </span>
                         <span className='text-blue-400'>
-                            Machine learning develop
+                            Koderx developers
                         </span>
                         <span className='text-blue-400'>
                             Applications desktop develop
