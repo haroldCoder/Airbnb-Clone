3fc455181edd31eb62f577f82a441a2c9c21cf35
haroldCoder <haroldc2005@gmail.com>
2023-04-19 13:37:01 -0500

    koderx link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ class Navbar extends Component{
                     </div>
                     <div className='flex space-x-4 items-center justify-end'>
                         <p className='hidden md:inline m-0'>Become a host</p>
-                        <GlobeAltIcon className='h-6 cursor-pointer'/>
+                        <a target='_blank' href='https://website-harold-koderx.netlify.app/'><GlobeAltIcon className='h-6 cursor-pointer'/></a>
                         <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
                             <MenuIcon className='h-6'/>
                             <UserCircleIcon className='h-6'/>
